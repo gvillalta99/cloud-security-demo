de4cec6a24830cce67fbddd42d086805324520e0
Gustavo Villalta <gvillalta99@users.noreply.github.com>
2021-04-29 03:36:07 -0300

    fix typo

--- network/outputs.tf
+++ network/outputs.tf
@@ -11,11 +11,11 @@ output "private_db_subnet_cidrs" {
   value = aws_subnet.private_db.*.cidr_block
 }
 output "public_subnet_ids" {
-  value = aws_subne.private_app.*.id
+  value = aws_subnet.private_app.*.id
 }
 output "private_app_subnet_ids" {
-  value = aws_subne.private_app.*.id
+  value = aws_subnet.private_app.*.id
 }
 output "private_db_subnet_ids" {
-  value = aws_subne.private_app.*.id
+  value = aws_subnet.private_app.*.id
 }
