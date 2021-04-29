2da1b5208c9967ccddffc8920b51ae796ce98cef
Gustavo Villalta <gvillalta99@users.noreply.github.com>
2021-04-29 02:22:53 -0300

    Fix all network plan errors

--- network/main.tf
+++ network/main.tf
@@ -10,8 +10,9 @@ provider "aws" {
   region = var.region
 }
 locals {
-  subnets         = cidrsubnets(var.cidr_block, var.newbits, var.newbits, var.newbits, var.newbits)
-  private_subnets = length(var.cidr_private_subnets) > 0 ? var.cidr_private_subnets : slice(local.subnets, 0, 1)
-  public_subnets  = length(var.cidr_public_subnets) > 0 ? var.cidr_public_subnets : slice(local.subnets, 2, 3)
-  azs_count       = length(var.availability_zones)
+  subnets             = cidrsubnets(var.cidr_block, var.newbits, var.newbits, var.newbits, var.newbits, var.newbits, var.newbits)
+  public_subnets      = length(var.cidr_public_subnets) > 0 ? var.cidr_public_subnets : slice(local.subnets, 2, 3)
+  private_app_subnets = length(var.cidr_private_app_subnets) > 0 ? var.cidr_private_app_subnets : slice(local.subnets, 0, 1)
+  private_db_subnets  = length(var.cidr_private_db_subnets) > 0 ? var.cidr_private_db_subnets : slice(local.subnets, 0, 1)
+  azs_count           = length(var.availability_zones)
 }
--- network/outputs.tf
+++ network/outputs.tf
@@ -5,8 +5,8 @@ output "public_subnet_cidrs" {
   value = aws_subnet.public.*.cidr_block
 }
 output "private_app_subnet_cidrs" {
-  value = aws_subnet.private.*.cidr_block
+  value = aws_subnet.private_app.*.cidr_block
 }
 output "private_db_subnet_cidrs" {
-  value = aws_subnet.private.*.cidr_block
+  value = aws_subnet.private_db.*.cidr_block
 }
